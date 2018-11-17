516336f329057419f5150ea649a12d4fdf9c12d4
HabibullinIlya <gimbarr12341@gmail.com>
2018-11-18 00:05:48 +0400

    init commit

---
+++ src/main/kotlin/xyz/ilyaxabibullin/templatesengine/ServletInitializer.kt
@@ -0,0 +1,12 @@
+package xyz.ilyaxabibullin.templatesengine
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+class ServletInitializer : SpringBootServletInitializer() {
+
+	override fun configure(application: SpringApplicationBuilder) : SpringApplicationBuilder {
+		return application.sources(DemoApplication::class.java)
+	}
+
+}
